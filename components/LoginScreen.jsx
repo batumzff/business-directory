83fde79dc971f970f8b0e0de624aca36cfae4bff
batumzff <batuhanmuzafferoglu@gmail.com>
2025-01-03 00:05:58 +0300

    login page

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/Colors";
 
 export default function LoginScreen() {
   return (
     <View>
-      <Text style={{fontSize:30}}>LoginScreen</Text>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
+        <Image
+          source={require("./../assets/images/login.png")}
+          style={{
+            width: 220,
+            height: 450,
+            borderRadius: 20,
+            borderWidth: 6,
+            borderColor: "#000",
+          }}
+        />
+      </View>
+
+      <View style={styles.subContainer}>
+        <Text style={{
+            fontSize:30,
+            fontFamily:'outfit-bold',
+            textAlign:'center'
+        }}>
+          Your Ultimate
+          <Text
+            style={{
+              color: Colors.PRIMARY,
+              
+            }}
+          >
+            Community Business Directory
+          </Text>
+          App
+        </Text>
+        <Text style={{
+            fontSize:15,
+            fontFamily:'outfit',
+            textAlign:'center',
+            marginVertical:15,
+            color:Colors.GRAY
+        }}>Find your favorite business near your and post your own business to your community</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+    subContainer:{
+        backgroundColor: "#fff",
+        padding:20,
+        marginTop:-20
+    }
+})
